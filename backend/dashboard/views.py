0579162e5991708b8c5ca7b1be3a7de1daf23b51
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-02-01 15:07:43 +0600

    Dashboard monthly analytics UI added with backend

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -9,6 +9,8 @@ from django.utils import timezone
 def dashboard_view(request):
 
     today = timezone.now().date()
+
+    # -------- WEEKLY --------
     week_ago = today - timedelta(days=6)
 
     weekly_expenses = Expense.objects.filter(
@@ -28,10 +30,33 @@ def dashboard_view(request):
 
     biggest_day = daily.order_by("-total").first()
 
+    # -------- MONTHLY --------
+
+    monthly_expenses = Expense.objects.filter(
+        user=request.user,
+        created_at__month=today.month,
+        created_at__year=today.year
+    )
+
+    monthly_total = monthly_expenses.aggregate(Sum("amount"))["amount__sum"] or 0
+
+    categories = (
+        monthly_expenses
+        .values("category")
+        .annotate(total=Sum("amount"))
+        .order_by("-total")
+    )
+
+    biggest_category = categories.first()
+
     context = {
         "weekly_total": round(weekly_total, 2),
         "daily": daily,
         "biggest_day": biggest_day,
+
+        "monthly_total": round(monthly_total, 2),
+        "categories": categories,
+        "biggest_category": biggest_category,
     }
 
     return render(request, "dashboard.html", context)
